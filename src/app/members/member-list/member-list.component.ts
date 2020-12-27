f87e4cc1e8cf30718564ac577877769f40d7c9ed
ivancica91 <a.ivancic91@gmail.com>
2020-12-27 21:33:41 +0100

    End of sect 10,Error when updating profile,fix it

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { MembersService } from './../../_services/members.service';
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
@@ -8,19 +9,21 @@ import { Member } from 'src/app/_models/member';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+ members$: Observable<Member[]> | undefined;
+  //members: Member[] = [];
 
   constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.membersService.getMembers();
+    //this.loadMembers();
   }
 
-  loadMembers() {
-    this.membersService.getMembers().subscribe(members => {
-      this.members = members;
-    })
+  // loadMembers() {
+  //   this.membersService.getMembers().subscribe(members => {
+  //     this.members = members;   //AL AKO OOV VRACAMO, NE BRISEMO GORNJI RED, TREBAJU NAM OBA
+  //   })
 
-  }
+  // }
 
 }
